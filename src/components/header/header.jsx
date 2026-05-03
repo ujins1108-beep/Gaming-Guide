4cbd5a9f54cb891c254a79f6c50b3ff5d1a6e6d9
XMFL <ujins1108@gmail.com>
2026-05-03 11:29:20 +0000

    이미지 표시문제 해결

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,7 +3,7 @@ import './header.css'
 export default function Header() {
     return (
         <div className='headerbox'>
-            <img className='logo' src='../../../public/ggl.png' />
+            <img className='logo' src='/ggl.png' />
             <h1 className='hometext'>Home</h1>
             <h1 className='abouttext'>About</h1>
         </div>
